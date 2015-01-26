790aa68e9f61add91b12309fa50cd9804369d10e
Frylle Banacso <akosifrylle@gmail.com>
2015-01-26 01:00:13 -0800

    Add function

--- MovieDB.Web/Scripts/app.js
+++ MovieDB.Web/Scripts/app.js
@@ -13,4 +13,10 @@ app.controller('SampleController', function($scope)
 {
     $scope.students = [{ name: "Frylle", course: "BIT" }, { name: "Jasper", course: "BIT" }];
     $scope.fruit = "Apple";
+    $scope.addStudent = function () {
+        $scope.students.push({
+            name: $scope.newStudent.name,
+            course: $scope.newStudent.course
+        });
+    };
 });
