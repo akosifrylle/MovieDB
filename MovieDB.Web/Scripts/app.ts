cfee2867e9cbf900c05e75e94026ca0f6cc9406e
Frylle Banacso <akosifrylle@gmail.com>
2015-01-27 23:41:35 -0800

    Converted app.js to Typescript

---
+++ MovieDB.Web/Scripts/app.ts
@@ -0,0 +1,21 @@
+﻿module  MovieDB {
+    var app = angular.module('movieDB', ['ngRoute']);
+
+    app.config(function($routeProvider) {
+        $routeProvider.when('/try', {
+            controller: 'SampleController',
+            templateUrl: 'PartialViews/List.html'
+        });
+    });
+
+    app.controller('SampleController', function ($scope) {
+        $scope.students = [{ name: "Frylle", course: "BIT" }, { name: "Jasper", course: "BIT" }];
+        $scope.fruit = "Apple";
+        $scope.addStudent = function () {
+            $scope.students.push({
+                name: $scope.newStudent.name,
+                course: $scope.newStudent.course
+            });
+        };
+    });
+} 
